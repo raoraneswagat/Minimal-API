5714a6f1a84b9b8c783077a38389c6331b87758b
swagat raorane <raorane.swagat@gmail.com>
2024-04-15 12:50:03 +0100

    MapGroup as an Extention methods

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -10,36 +10,7 @@ List<Product> products = new List<Product>(){
     new Product(){Id=1,Name="TV"},
 };
 
-var mapGroup = app.MapGroup("/products");
+var mapGroup = app.MapGroup("/products").ProductsAPI();
 
-mapGroup.MapGet("/",async (HttpContext context) => {
-
-string content = string.Join("\n",products.Select(s=>s.ToString()));
-await context.Response.WriteAsync(content);
-
-});
-
-mapGroup.MapGet("/{id:int}",async (HttpContext context,int id) => {
-
-Product? product = products.Where(w=>w.Id == id).FirstOrDefault();
-if(product==null)
-{
-    context.Response.StatusCode = 400;
-    await context.Response.WriteAsync("Incorrect Product Id");
-    return;
-}
-
-await context.Response.WriteAsync(JsonSerializer.Serialize(product));
-
-});
-
-
-mapGroup.MapPost("/",async (HttpContext context,Product product) => {
-
-products.Add(product);
-
-await context.Response.WriteAsync("Product Added");
-
-});
 
 app.Run();
---
+++ MinimalAPI/RouteGroups/ProductMapGroup.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+
+namespace MinimalAPI;
+
+public static class ProductMapGroup
+{
+    public static List<Product> products = new List<Product>(){
+
+    new Product(){Id=1,Name="Phone"},
+    new Product(){Id=1,Name="TV"},
+};
+    public static RouteGroupBuilder ProductsAPI(this RouteGroupBuilder group)
+    {
+
+        group.MapGet("/", async (HttpContext context) =>
+        {
+
+            string content = string.Join("\n", products.Select(s => s.ToString()));
+            await context.Response.WriteAsync(content);
+
+        });
+
+        group.MapGet("/{id:int}", async (HttpContext context, int id) =>
+        {
+
+            Product? product = products.Where(w => w.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Incorrect Product Id");
+                return;
+            }
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(product));
+
+        });
+
+
+        group.MapPost("/", async (HttpContext context, Product product) =>
+        {
+
+            products.Add(product);
+
+            await context.Response.WriteAsync("Product Added");
+
+        });
+
+        return group;
+    }
+}
